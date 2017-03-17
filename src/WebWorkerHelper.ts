c5154b0fe86c6e1e3b03095fe0b02e2617b12f35
Ivor, Xinzi Zhou <zhouxz@garena.com>
2017-03-17 12:43:19 +0800

    Change API.

--- src/WebWorkerHelper.ts
+++ src/WebWorkerHelper.ts
@@ -8,32 +8,60 @@ function workerScript() {
     const processor : FileWorkerProcessor = __PROCESSOR__;
 
     class FileWorkerReaderImplement implements FileWorkerReader {
+        onFinished: Function;
         onData: FileWorkerOnDataHandler;
         private file : File;
 
+        private currentReader : number;
+
         constructor(file: File) {
             this.file = file;
+            this.currentReader = 0;
+        }
+
+        read(length:number) {
+            const maxLength = Math.min(this.file.size, this.currentReader + length);
+            if (this.currentReader == this.file.size) {
+                this.onFinished();
+            } else {
+                const blob = this.file.slice(this.currentReader, maxLength);
+                this.currentReader = maxLength;
+                const fileReader = new FileReaderSync();
+                const arrayBuffer = fileReader.readAsArrayBuffer(blob);
+                this.onData(arrayBuffer);
+                if (this.currentReader == this.file.size) {
+                    this.onFinished();
+                }
+            }
         }
 
         readAll() {
             const fileReader = new FileReaderSync();
             const arrayBuffer = fileReader.readAsArrayBuffer(this.file);
             this.onData(arrayBuffer);
+            this.onFinished();
         }
     }
 
     self.addEventListener('message', (e : MessageEvent) => {
         const file: File = <File>e.data;
+        const writerBuffer : any[] = [];
         const reader : FileWorkerReader = <FileWorkerReader>new FileWorkerReaderImplement(file);
         const writer : FileWorkerWriter = <FileWorkerWriter>{
             write: (data: any) => {
+                writerBuffer.push(data);
+            },
+            writeOnce: (data: any) => {
                 self.postMessage(data);
+            },
+            finish: () => {
+                self.postMessage(writerBuffer);
             }
         };
         try {
             processor(reader, writer);
         } catch (e) {
-            self.postMessage(e);
+            self.postMessage({error: e.toString()});
         }
     });
 }
--- test/browser/index.spec.js
+++ test/browser/index.spec.js
@@ -6,9 +6,93 @@ describe('FileWorker', () => {
 
   describe('readFile', () => {
     it('should return a Promise.', () => {
-      var response = FileWorker.readFile();
+      let response = FileWorker.readFile(null, function(){});
       expect(typeof response).toEqual('object');
       expect(response.constructor.name).toEqual('Promise');
+    });
+  });
+
+  describe('Writer', () => {
+    it('writes once should work.', (done) => {
+      let string = 'abcdefg';
+      let file = new Blob([string], {type: 'text/plain'});
+      file.lastModifiedDate = new Date();
+      file.name = 'dummy';
+
+
+      function byteProcessor(reader, writer) {
+        reader.onData = (data) => {
+          writer.writeOnce(data.byteLength);
+        };
+        reader.readAll();
+      }
+
+      let response = FileWorker.readFile(file, byteProcessor);
+      response.then((result) => {
+        expect(result).toEqual(7);
+        done();
+      });
+
+    });
+
+    it('writes many times should work.', (done) => {
+      let string = 'abcdefg';
+      let file = new Blob([string], {type: 'text/plain'});
+      file.lastModifiedDate = new Date();
+      file.name = 'dummy';
+
+
+      function byteProcessor(reader, writer) {
+        reader.onData = (data) => {
+          writer.write(data.byteLength);
+          writer.write("hello world");
+          writer.finish();
+          writer.write("hello again.");
+          writer.finish();
+        };
+        reader.readAll();
+      }
+
+      let response = FileWorker.readFile(file, byteProcessor);
+      response.then((result) => {
+        expect(result.length).toEqual(2);
+        expect(result[0]).toEqual(7);
+        expect(result[1]).toEqual("hello world");
+        done();
+      });
+
+    });
+
+  });
+
+
+
+  describe('Reader', () => {
+    it('read multiple times should work.', (done) => {
+      let string = 'abcdefg';
+      let file = new Blob([string], {type: 'text/plain'});
+      file.lastModifiedDate = new Date();
+      file.name = 'dummy';
+
+      function byteProcessor(reader, writer) {
+        reader.onData = (data) => {
+          writer.write(data.byteLength);
+          reader.read(2);
+        };
+        reader.onFinished = () => {
+          writer.finish();
+        };
+        reader.read(2);
+      }
+
+      let response = FileWorker.readFile(file, byteProcessor);
+      response.then((result) => {
+        expect(result.length).toEqual(4);
+        expect(result[0]).toEqual(2);
+        expect(result[3]).toEqual(1);
+        done();
+      });
+
     });
   });
 
