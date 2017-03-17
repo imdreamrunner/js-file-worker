c61d230b844bcb32eb60e3ebc7b702673e1531af
Ivor, Xinzi Zhou <imdreamrunner@gmail.com>
2017-03-17 19:05:37 +0800

    Remove console.log.

--- src/FileWorker.ts
+++ src/FileWorker.ts
@@ -3,7 +3,6 @@ import * as WebWorkerHelper from "./WebWorkerHelper";
 
 export class FileWorker {
     static async readFile(file : File, processor : FileWorkerProcessor) : Promise<any> {
-        console.log(file);
         const result : Promise<any> = new Promise<any>((resolve, reject) => {
             const worker : Worker = WebWorkerHelper.createWebWorker(processor);
             worker.postMessage(file);
